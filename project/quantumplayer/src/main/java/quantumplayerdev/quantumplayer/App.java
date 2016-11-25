ee36487ab1eb7c02997b6c8c09041beb7ed38910
leonardo <reger@Leonardo-Laptop.reger.lan>
2016-11-25 11:37:45 +0100

    inital

---
+++ project/quantumplayer/src/main/java/quantumplayerdev/quantumplayer/App.java
@@ -0,0 +1,13 @@
+package quantumplayerdev.quantumplayer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
