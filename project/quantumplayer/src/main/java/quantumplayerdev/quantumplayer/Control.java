3a6463d19d2a7e07a7eef62590a7cacfdc9e354f
leonardo <reger@Leonardo-Laptop.reger.lan>
2016-11-28 13:52:48 +0100

    Read mp3s

--- project/quantumplayer/src/main/java/quantumplayerdev/quantumplayer/Control.java
+++ project/quantumplayer/src/main/java/quantumplayerdev/quantumplayer/Control.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 import org.slf4j.LoggerFactory;
 
+import com.mpatric.mp3agic.ID3v1;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.Mp3File;
+
 public class Control {
 	org.slf4j.Logger logger = LoggerFactory.getLogger(Control.class);
 
@@ -14,14 +18,34 @@ public class Control {
 		List<File> files = new ArrayList<File>();
 		files.add(new File("C:\\Users\\reger"));
 		ctr.setFolders(files);
+		ctr.loadFiles();
 	}
 
-	private List<File> songFiles = new ArrayList<File>();
+	private List<Mp3File> songsMP3 = new ArrayList<Mp3File>();
 	private List<Song> songs = new ArrayList<Song>();
 
 	public void setFolders(List<File> files) {
 		File[] array = new File[files.size()];
 		addFiles(files.toArray(array));
+	}
+
+	public List<Song> loadFiles() {
+		for (Mp3File mp3File : songsMP3) {
+			Song song = new Song();
+			if (mp3File.hasId3v1Tag()) {
+				ID3v1 tag = mp3File.getId3v1Tag();
+				song.setTitle(tag.getTitle());
+				song.setAlbum(tag.getAlbum());
+			} else if (mp3File.hasId3v2Tag()) {
+				ID3v2 tag = mp3File.getId3v2Tag();
+				song.setTitle(tag.getTitle());
+				song.setAlbum(tag.getAlbum());
+			} else {
+
+			}
+			songs.add(song);
+		}
+		return songs;
 	}
 
 	private void addFiles(File[] files) {
@@ -32,7 +56,7 @@ public class Control {
 						addFiles(file.listFiles());
 					} else {
 						if (file.getPath().endsWith(".mp3")) {
-							songFiles.add(file);
+							songsMP3.add(new Mp3File(file));
 						}
 					}
 				}
