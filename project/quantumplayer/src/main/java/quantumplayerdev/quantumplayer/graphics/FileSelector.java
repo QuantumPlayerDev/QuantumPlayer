8cb6802f96e803efb91ab448aafdc4c26f1683f4
Daniel <weberdaniel947@gmail.com>
2016-11-29 09:18:49 +0100

    FileSelectorUpdate

--- project/quantumplayer/src/main/java/quantumplayerdev/quantumplayer/graphics/FileSelector.java
+++ project/quantumplayer/src/main/java/quantumplayerdev/quantumplayer/graphics/FileSelector.java
@@ -1,43 +1,54 @@
 package quantumplayerdev.quantumplayer.graphics;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableColumn;
-import javafx.scene.control.TreeTableView;
-import javafx.scene.control.cell.CheckBoxTreeTableCell;
-import javafx.scene.control.TreeTableColumn.CellDataFeatures;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafx.stage.Window;
-import javafx.util.Callback;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.CheckBoxTreeCell;
 
-public class FileSelector extends Stage {
+/**
+ * The FileSelector class creates a new {@link TreeView} object that
+ * contains {@link FileView} object to represent the structure of all
+ * {@link File} objects that exist on a local device.
+ * @author daniel
+ *
+ */
+public class FileSelector extends TreeView<FileView> {
 	
-	private TreeTableView<File> tableView;
-	
-	public FileSelector(Window window) {
+	/**
+	 * Constructor for creating a new {@link FileSelector} object
+	 * to represent all files that exist depending on the {@link #rootFile}.
+	 * @param rootFile The root file to set.
+	 */
+	public FileSelector(File rootFile) {
 		super();
-		this.initModality(Modality.WINDOW_MODAL);
-		this.initOwner(window);
-		this.tableView = new TreeTableView<File>();
-		
-		final TreeItem<String> columnName = new TreeItem<String>();
-//		columnName.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<File,String>, ObservableValue<String>>() {
-//			
-//			public ObservableValue<String> call(CellDataFeatures<File, String> param) {
-//				return new SimpleStringProperty(param.getValue().getValue().getName());
-//			}
-//		});
-//		final TreeTableColumn<File, Boolean> columnSelect = new TreeTableColumn<File, Boolean>();
-//		columnSelect.setPrefWidth(75.0);
-//		columnSelect.setStyle("-fx-alignment: CENTER-RIGHT;");
-//		columnSelect.setCellFactory(CheckBoxTreeTableCell.forTreeTableColumn(columnSelect));
+		this.setRoot(new FileSystemView(new FileView(rootFile)));
+		this.setCellFactory(CheckBoxTreeCell.<FileView>forTreeView());
+		style();
 	}
 	
-	private void findFile(File dir, TreeItem<File> parent) {
-		TreeItem<File> root = new TreeItem<File>(dir);
+	/**
+	 * Adds Cascading Style sheet attributes to this {@link TreeView} object.
+	 */
+	private void style() {
+		this.setStyle("-fx-font-weight: bold; -fx-background-color: black;"
+				+ "-fx-text-fill: white;");
+	}
+	
+	/**
+	 * Gets the {@link File} objects that have been selected in this
+	 * {@link TreeView} object.
+	 * @return a list of {@link File} objects.
+	 */
+	public List<File> getSelection() {
+		final ObservableList<TreeItem<FileView>> views = this.getSelectionModel().getSelectedItems();
+		final List<File> selectedFiles = new ArrayList<File>();
+		for (int i = 0; i < views.size(); i++) {
+			selectedFiles.add(views.get(i).getValue().getFile());
+		}
+		return selectedFiles;
 	}
 }
