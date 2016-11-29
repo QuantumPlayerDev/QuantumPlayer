4e2f12dd3713e87d61410fc4189c07f77bcba708
Jonas Knapp <de.jonas.knapp@gmail.com>
2016-11-29 22:07:28 +0100

    buggy merge

--- project/quantumplayer/src/main/java/quantumplayerdev/quantumplayer/graphics/FileSelector.java
+++ project/quantumplayer/src/main/java/quantumplayerdev/quantumplayer/graphics/FileSelector.java
@@ -10,18 +10,21 @@ import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.CheckBoxTreeCell;
 
 /**
- * The FileSelector class creates a new {@link TreeView} object that
- * contains {@link FileView} object to represent the structure of all
- * {@link File} objects that exist on a local device.
+ * The FileSelector class creates a new {@link TreeView} object that contains
+ * {@link FileView} object to represent the structure of all {@link File}
+ * objects that exist on a local device.
+ * 
  * @author daniel
  *
  */
 public class FileSelector extends TreeView<FileView> {
-	
+
 	/**
-	 * Constructor for creating a new {@link FileSelector} object
-	 * to represent all files that exist depending on the {@link #rootFile}.
-	 * @param rootFile The root file to set.
+	 * Constructor for creating a new {@link FileSelector} object to represent
+	 * all files that exist depending on the {@link #rootFile}.
+	 * 
+	 * @param rootFile
+	 *            The root file to set.
 	 */
 	public FileSelector(File rootFile) {
 		super();
@@ -29,18 +32,18 @@ public class FileSelector extends TreeView<FileView> {
 		this.setCellFactory(CheckBoxTreeCell.<FileView>forTreeView());
 		style();
 	}
-	
+
 	/**
 	 * Adds Cascading Style sheet attributes to this {@link TreeView} object.
 	 */
 	private void style() {
-		this.setStyle("-fx-font-weight: bold; -fx-background-color: black;"
-				+ "-fx-text-fill: white;");
+		this.setStyle("-fx-font-weight: bold; -fx-background-color: black;" + "-fx-text-fill: white;");
 	}
-	
+
 	/**
 	 * Gets the {@link File} objects that have been selected in this
 	 * {@link TreeView} object.
+	 * 
 	 * @return a list of {@link File} objects.
 	 */
 	public List<File> getSelection() {
