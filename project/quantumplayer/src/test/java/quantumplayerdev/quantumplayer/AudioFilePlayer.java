39144b5ef887906268d029034c3f3a248fc5d606
Thallosaurier <de.jonas.knapp@gmail.com>
2016-12-02 14:00:37 +0100

    better audiofileplayer-test

--- project/quantumplayer/src/test/java/quantumplayerdev/quantumplayer/AudioFilePlayer.java
+++ project/quantumplayer/src/test/java/quantumplayerdev/quantumplayer/AudioFilePlayer.java
@@ -9,15 +9,22 @@ import java.util.List;
 
 import org.junit.Test;
 
+import com.sun.xml.internal.ws.api.addressing.WSEndpointReference.Metadata;
+
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class AudioFilePlayer extends Application {
+	
+	private MediaPlayer player;
 
 	@Test
 	public void run() {
@@ -37,26 +44,43 @@ public class AudioFilePlayer extends Application {
 
 		System.out.println("----------------------------");
 		System.out.println("Found songs: " + songs.size());
-		for (Song song : songs) {
-			System.out.println("Title: " + song.getTitle());
-		}
-		SongPlayer player = new SongPlayer();
-		System.out.println(songs.get(0).getPath());
 		
-		Button playButton = new Button("Play");
-		playButton.setOnAction(new EventHandler<ActionEvent>() {
-		    @Override public void handle(ActionEvent e) {
+		FlowPane root = new FlowPane();
+		Button stopButton = new Button("stop");
+		stopButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent e) {
 				Thread thread = new Thread( new Runnable() {
 					@Override
 					public void run() {
-						player.playSong(songs.get(0));
+						player.stop();
 					}
 				});
 				thread.start();
-		    }
+			}
 		});
-        StackPane root = new StackPane();
-        root.getChildren().add(playButton);
+		root.getChildren().add(stopButton);
+		for (Song song : songs) {
+//			System.out.println("Title: " + song.getTitle());
+			
+			Button playButton = new Button(song.getTitle());
+			playButton.setOnAction(new EventHandler<ActionEvent>() {
+				@Override public void handle(ActionEvent e) {
+					Thread thread = new Thread( new Runnable() {
+						@Override
+						public void run() {
+							//player.playSong(songs.get(0));
+							File mediaFile = new File(songs.get(songs.indexOf(song)).getPath());
+							System.out.println(mediaFile.getPath());
+							Media song = new Media(mediaFile.toURI().toASCIIString());
+							player = new MediaPlayer(song);
+							player.play();
+						}
+					});
+					thread.start();
+				}
+			});
+			root.getChildren().add(playButton);
+		}
         Scene scene = new Scene(root, 300, 250);
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
